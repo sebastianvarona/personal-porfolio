7a4da9fcb2a223223a7705fd5ff80d005cf38535
Sebastian Varona <sebastianvarona@gmail.com>
2022-07-11 04:05:05 -0500

    deploy.sh Updated

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:sebastianvarona/sebastianvarona.github.io.git main
+# git push -f git@github.com:sebastianvarona/sebastianvarona.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:sebastianvarona/personal-porfolio.git main:gh-pages
 
 cd -
